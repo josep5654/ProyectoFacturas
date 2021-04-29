d32da073ff96fcd59b475eeaa73cdb2e699328ec
José Pablo Castillo Pérez <josepablo.castillo@bosonit.com>
2021-04-29 10:22:43 +0200

    cambio en producto y clientes

--- src/main/java/com/springteam/proyectofacturas/Entities/Cliente/Cliente.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/Cliente/Cliente.java
@@ -1,4 +1,23 @@
 package com.springteam.proyectofacturas.Entities.Cliente;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nombreCliente;
+
 }
--- src/main/java/com/springteam/proyectofacturas/Entities/Producto/Producto.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/Producto/Producto.java
@@ -1,4 +1,20 @@
 package com.springteam.proyectofacturas.Entities.Producto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Producto {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nombreProducto;
 }

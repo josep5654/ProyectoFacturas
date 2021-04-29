741f5bf3bbbbfe9e4ba0c21c9414518b31d5c6ce
javier <jgg8340@gmail.com>
2021-04-29 09:54:50 +0200

    First Commit

---
+++ src/main/java/com/springteam/proyectofacturas/ProyectoFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.springteam.proyectofacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFacturasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFacturasApplication.class, args);
+    }
+
+}

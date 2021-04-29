6fb3ced0caa330a59c5216ba2dd59c0772bc3cb5
javier <jgg8340@gmail.com>
2021-04-29 15:24:36 +0200

    add getById to Service LineaFactura

--- src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Services/ILineaFacturaService.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Services/ILineaFacturaService.java
@@ -1,4 +1,10 @@
 package com.springteam.proyectofacturas.Entities.LineaFactura.Services;
 
+
+import com.springteam.proyectofacturas.Entities.LineaFactura.Model.LineaFacturaDTO;
+import javassist.NotFoundException;
+
 public interface ILineaFacturaService {
+
+    public LineaFacturaDTO getLineaFacturaById(Integer id) throws NotFoundException;
 }

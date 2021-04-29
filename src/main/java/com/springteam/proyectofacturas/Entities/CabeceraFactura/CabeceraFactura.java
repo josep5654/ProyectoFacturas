637dcd2e1ebefbffcf75b3828f55d7b47a5e0fd9
javier <jgg8340@gmail.com>
2021-04-29 10:32:25 +0200

    add CabeceraFactura properties

--- src/main/java/com/springteam/proyectofacturas/Entities/CabeceraFactura/CabeceraFactura.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/CabeceraFactura/CabeceraFactura.java
@@ -1,4 +1,36 @@
 package com.springteam.proyectofacturas.Entities.CabeceraFactura;
 
+import com.springteam.proyectofacturas.Entities.Cliente.Cliente;
+import com.springteam.proyectofacturas.Entities.LineaFactura.LineaFactura;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class CabeceraFactura {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NonNull
+    private String numFactura;
+
+
+    private Date fecha;
+
+    @NonNull
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Cliente cliente;
+
+//    @OneToMany(cascade = CascadeType.ALL)
+//    private List<LineaFactura> lineaFacturas;
 }

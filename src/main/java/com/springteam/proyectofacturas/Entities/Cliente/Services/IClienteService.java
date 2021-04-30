fc4f5919f1582a432db1f580db4c6f9e38b55a3f
javier <jgg8340@gmail.com>
2021-04-30 10:48:07 +0200

    add methoods to clientService

--- src/main/java/com/springteam/proyectofacturas/Entities/Cliente/Services/IClienteService.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/Cliente/Services/IClienteService.java
@@ -4,6 +4,16 @@ import com.springteam.proyectofacturas.Entities.CabeceraFactura.model.CabeceraFa
 import com.springteam.proyectofacturas.Entities.Cliente.Model.ClienteDTO;
 import javassist.NotFoundException;
 
+import java.util.List;
+
 public interface IClienteService {
+
+
     public ClienteDTO getClienteById(Integer id) throws NotFoundException;
+    public List<ClienteDTO> getAllClientes();
+    public void updateCliente(Integer id,ClienteDTO clienteDTO) throws NotFoundException;
+    public void deleteCliente(Integer id);
+    public void createCliente(ClienteDTO clienteDTO);
+
+
 }

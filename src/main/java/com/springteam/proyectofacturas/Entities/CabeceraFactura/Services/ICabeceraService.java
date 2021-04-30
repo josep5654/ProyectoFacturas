3122fe744dfa78523f7fb56d013c32dd72dad205
javier <jgg8340@gmail.com>
2021-04-30 10:31:16 +0200

    add methods to Interface CabeceraService

--- src/main/java/com/springteam/proyectofacturas/Entities/CabeceraFactura/Services/ICabeceraService.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/CabeceraFactura/Services/ICabeceraService.java
@@ -2,12 +2,17 @@ package com.springteam.proyectofacturas.Entities.CabeceraFactura.Services;
 
 import com.springteam.proyectofacturas.Entities.CabeceraFactura.model.CabeceraFacturaDTO;
 import javassist.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 public interface ICabeceraService {
 
 
     public CabeceraFacturaDTO getCabeceraById(Integer id) throws NotFoundException;
+    public List<CabeceraFacturaDTO> getAllCabeceras();
+    public void updateCabecera(Integer id,CabeceraFacturaDTO cabeceraFacturaDTO) throws NotFoundException;
+    public void deleteCabecera(Integer id);
+    public void createCabecera(Integer id,CabeceraFacturaDTO cabeceraFacturaDTO);
+
 }

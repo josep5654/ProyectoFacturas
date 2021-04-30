676ff15b4d706088ef0b7afd4457912ad7dc5903
javier <jgg8340@gmail.com>
2021-04-30 12:42:36 +0200

    add properties to LineaFactura

--- src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Model/LineaFactura.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Model/LineaFactura.java
@@ -1,4 +1,35 @@
 package com.springteam.proyectofacturas.Entities.LineaFactura.Model;
 
+import com.springteam.proyectofacturas.Entities.CabeceraFactura.model.CabeceraFactura;
+import com.springteam.proyectofacturas.Entities.Producto.Model.Producto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class LineaFactura {
+
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    private CabeceraFactura cabeceraFactura;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @NonNull
+    private Producto producto;
+
+    @NonNull
+    private Integer cantidad;
+
+    @NonNull
+    private Integer precio;
 }
--- src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Model/LineaFacturaDTO.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Model/LineaFacturaDTO.java
@@ -1,4 +1,19 @@
 package com.springteam.proyectofacturas.Entities.LineaFactura.Model;
 
+import com.springteam.proyectofacturas.Entities.CabeceraFactura.model.CabeceraFactura;
+import com.springteam.proyectofacturas.Entities.Producto.Model.Producto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
 public class LineaFacturaDTO {
+
+
+    private Integer id;
+    private CabeceraFactura cabeceraFactura;
+    private Producto producto;
+    private Integer cantidad;
+    private Integer precio;
 }

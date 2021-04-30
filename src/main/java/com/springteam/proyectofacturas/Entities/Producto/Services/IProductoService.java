64dfb85c90a2beaaa6dad8f5e371d48f243e1859
javier <jgg8340@gmail.com>
2021-04-30 10:52:46 +0200

    add methoods to IProductoService

--- src/main/java/com/springteam/proyectofacturas/Entities/Producto/Services/IProductoService.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/Producto/Services/IProductoService.java
@@ -1,4 +1,17 @@
 package com.springteam.proyectofacturas.Entities.Producto.Services;
 
+
+import com.springteam.proyectofacturas.Entities.Producto.Model.ProductoDTO;
+import javassist.NotFoundException;
+
+import java.util.List;
+
 public interface IProductoService {
+
+    public ProductoDTO getProductoById(Integer id) throws NotFoundException;
+
+    public List<ProductoDTO> getAllLineaProductos();
+    public void updateLineaProducto(Integer id,ProductoDTO productoDTO) throws NotFoundException;
+    public void deleteLineaProducto(Integer id);
+    public void createLineaProducto(ProductoDTO productoDTO);
 }

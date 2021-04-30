5e416b0c620f76df6490031e18253b98919be4af
javier <jgg8340@gmail.com>
2021-04-30 10:50:21 +0200

    add methoods to ILineaFacturaService

--- src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Services/ILineaFacturaService.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/LineaFactura/Services/ILineaFacturaService.java
@@ -1,10 +1,21 @@
 package com.springteam.proyectofacturas.Entities.LineaFactura.Services;
 
 
+
+import com.springteam.proyectofacturas.Entities.CabeceraFactura.model.CabeceraFacturaDTO;
 import com.springteam.proyectofacturas.Entities.LineaFactura.Model.LineaFacturaDTO;
 import javassist.NotFoundException;
+
+import java.util.List;
 
 public interface ILineaFacturaService {
 
     public LineaFacturaDTO getLineaFacturaById(Integer id) throws NotFoundException;
+    public LineaFacturaDTO getLineaFacturaByHeader(CabeceraFacturaDTO cabeceraFacturaDTO) throws NotFoundException;
+    public List<LineaFacturaDTO> getAllLineaFacturas();
+    public void updateLineaFactura(Integer id,LineaFacturaDTO lineaFacturaDTO) throws NotFoundException;
+    public void deleteLineaFactura(Integer id);
+    public void createLineaFactura(LineaFacturaDTO lineaFacturaDTO);
+
+
 }

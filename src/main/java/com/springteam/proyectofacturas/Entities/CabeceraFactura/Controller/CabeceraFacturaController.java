72b3c9bacd37a8e9786e62cc0531259322bb160d
javier <jgg8340@gmail.com>
2021-04-30 10:31:43 +0200

    add Crud methods to Controller

--- src/main/java/com/springteam/proyectofacturas/Entities/CabeceraFactura/Controller/CabeceraFacturaController.java
+++ src/main/java/com/springteam/proyectofacturas/Entities/CabeceraFactura/Controller/CabeceraFacturaController.java
@@ -6,6 +6,8 @@ import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/factura")
 public class CabeceraFacturaController {
@@ -23,6 +25,37 @@ public class CabeceraFacturaController {
             return null;
         }
 
+    }
+    @GetMapping("/")
+    public List<CabeceraFacturaDTO> getAllCabecera(@PathVariable String id)  {
+        return cabeceraService.getAllCabeceras();
+    }
+
+    @PostMapping("/")
+    public String addCabecera(@RequestBody CabeceraFacturaDTO cabeceraFacturaDTO)  {
+
+
+        return "ok";
+    }
+    @DeleteMapping("/{id}")
+    public String removeCabecera(@PathVariable String id)  {
+
+        cabeceraService.deleteCabecera(Integer.parseInt(id));
+
+        return "ok";
+    }
+
+    @PutMapping("/{id}")
+    public String editCabecera(@PathVariable String id, @RequestBody CabeceraFacturaDTO cabeceraFacturaDTO)  {
+
+        try {
+            cabeceraService.updateCabecera(Integer.parseInt(id),cabeceraFacturaDTO);
+        }catch (NotFoundException e){
+
+        }
+
+
+        return "ok";
     }
 
 
